69f56cc11d30bc4c059d272ce25659e7d783682b
Nicolas <nicolascharpentier1808@gmail.com>
2025-12-23 01:17:20 +0100

    fix: merged DatabaseConnection with DatabaseLogsConnection

--- src/main/java/appli/config/DatabaseConnection.java
+++ src/main/java/appli/config/DatabaseConnection.java
@@ -6,11 +6,26 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    public static Connection getConnexion() {
+    public static Connection getMainConnexion() {
         Connection cnx = null;
         try {
             cnx = DriverManager.getConnection(
                     DatabaseConfig.getUrlMain(),
+                    DatabaseConfig.getUsername(),
+                    DatabaseConfig.getPassword()
+            );
+            System.out.println("Connexion réussie à la base de données !");
+        } catch (SQLException e) {
+            System.out.println("Erreur de connexion : " + e.getMessage());
+        }
+        return cnx;
+    }
+
+    public static Connection getLogsConnexion() {
+        Connection cnx = null;
+        try {
+            cnx = DriverManager.getConnection(
+                    DatabaseConfig.getUrlLogs(),
                     DatabaseConfig.getUsername(),
                     DatabaseConfig.getPassword()
             );
