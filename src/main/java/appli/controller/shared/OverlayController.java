06ac58d2110fe6c556eda5ec23010cd1684b5057
Nicolas <nicolascharpentier1808@gmail.com>
2025-12-26 22:22:10 +0100

    wip: fxml overlay

--- src/main/java/appli/controller/shared/OverlayController.java
+++ src/main/java/appli/controller/shared/OverlayController.java
@@ -1,4 +1,57 @@
 package appli.controller.shared;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+
 public class OverlayController {
+
+    @FXML
+    private StackPane contentPane;
+
+    @FXML
+    private Label userLabel;
+
+
+    public void setUser(String username) {
+        userLabel.setText(username);
+    }
+
+    public StackPane getContentPane() {
+        return contentPane;
+    }
+
+    @FXML
+    public void initialize(){
+        load("/appli/main/login.fxml");
+    }
+
+    @FXML
+    private void onAccueil() {
+        load("/appli/main/login.fxml");
+        System.out.println("Accueil cliqué");
+    }
+
+    @FXML
+    private void onPatients() {
+        System.out.println("Patients cliqué");
+    }
+
+    @FXML
+    private void onStock() {
+        System.out.println("Stock cliqué");
+    }
+
+    private void load(String path){
+        try {
+            Parent view = FXMLLoader.load(getClass().getResource(path));
+            contentPane.getChildren().setAll(view);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
 }
