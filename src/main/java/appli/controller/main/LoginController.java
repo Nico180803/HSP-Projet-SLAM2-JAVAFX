65a2edd36c476e38d0476c8976e6b7e87f27ed54
Ademe <A.AYAD@lprs.fr>
2026-01-27 11:48:11 +0100

    [fix] Modification Connexion (controller + fxml)

--- src/main/java/appli/controller/main/LoginController.java
+++ src/main/java/appli/controller/main/LoginController.java
@@ -1,8 +1,55 @@
 package appli.controller.main;
 
+import appli.dao.principal.jdbc.UtilisateurDAO;
+import appli.model.principal.Utilisateur;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import session.SessionUtilisateur;
+
+import java.io.IOException;
+
 import static java.lang.System.load;
 
 public class LoginController {
+    private final UtilisateurDAO repo = new UtilisateurDAO();
+    @FXML
+    private Button connexionbutton;
+
+    @FXML
+    private Button pageInscriptionButton;
+
+    @FXML
+    private Button mdpOblieButton;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField mdpField;
+
+    void handleLogin(ActionEvent event) throws IOException {
+        System.out.println("Email: " + emailField.getText());
+        System.out.println("Password: " + mdpField.getText());
+
+        String email = emailField.getText();
+        String password = mdpField.getText();
+
+        if (email.isEmpty() || password.isEmpty()) {
+            System.out.println("Tous les champs n'ont pas été remplis !");
+        } else {
+            Utilisateur utilisateur = repo.getByEmail(email);
+            if (utilisateur != null) {
+                System.out.println("Connexion réussie pour : " + utilisateur.getNom());
+                SessionUtilisateur.getInstance().sauvegardeSession(utilisateur);
+            } else {
+                System.out.println("Échec de la connexion. Email ou mot de passe incorrect.");
+            }
+        }
+    }
+
     public void onPageInscriptionButtonClick() {
         load("/appli/patient/TableFichePatient.fxml");
         System.out.println("Arrivée sur la page d'inscription");
